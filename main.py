f03debaccefd644fd6f4bc7172d0118396b18960
Chris Owens <christophermichaelowens@gmail.com>
2017-10-05 19:42:05 -0500

    yay done

--- main.py
+++ main.py
@@ -18,20 +18,28 @@ def add_user():
 	error_email = ""
 	
 	
-	if len(username) < 3 or len(username) > 20 :
+	if len(username) < 3 or len(username) > 20 or " " in username:
 		error_user = "Please enter a username between 3 and 20 characters"
 		
 
-	if password!=verify:
+	if password!=verify or password == "":
 		error_verify = "Passwords don't match"
 		
 		
-	if len(password) < 3 or len(password) > 20:
+	if len(password) < 3 or len(password) > 20 or " " in password:
 		error_pass = "Please enter a password between 3 and 20 characters"
-		
 	
-	if error_user or error_pass or error_verify:
-		return render_template(username = username, email = email,error_user = error_user, error_pass = error_pass, error_verify = error_verify)
+	if "@" not in email or "." not in email or " " in email:
+		error_email = "You need a valid e-mail address."
+	
+	if len(email) < 3 or len(email) > 20 or " " in email:
+		error_email = "Email needs to be between 3 and 20 characters"
+	
+	if email == "":
+		error_email = ""
+	
+	if error_user != "" or error_pass != "" or error_verify != "" or error_email != "":
+		return render_template('index.html', username = username, email = email,error_user = error_user, error_pass = error_pass, error_verify = error_verify, error_email = error_email)
 		
 		
 	return render_template('welcome.html', username=username)
